9bff8469d047ad656e16c5c27644ba24748453ab
Carolyn Webster Pless <carolyn.webster-pless@stu.ocsb.ca>
2022-09-16 02:22:12 +0000

    coded message :)

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 Carolyn All rights reserved.
+
+//
+
+// Created by: Carolyn Webster Pless
+
+// Date: 09, 15, 2022
+
+// Displays the text: Hello World, with style!!!!!
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World, with style!!!!!\n";
+}
